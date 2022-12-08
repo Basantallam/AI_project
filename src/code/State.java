07a35b3e13015a18a68e47f9754fb8beb36fd41d
ayaelgamall <aya.elgamal14@gmail.com>
2022-12-08 19:48:17 +0200

    .Equals

--- src/code/State.java
+++ src/code/State.java
@@ -23,12 +23,12 @@ public class State {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         State state = (State) o;
-        return savedSoFar == state.savedSoFar && boxes == state.boxes && remCap == state.remCap && time <= state.time && pos.equals(state.pos);
+        return savedSoFar == state.savedSoFar && boxes == state.boxes && remCap == state.remCap && time >= state.time && pos.equals(state.pos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pos, savedSoFar, boxes, remCap, time);
+        return Objects.hash(pos, savedSoFar, boxes, remCap);
     }
 
     @Override
@@ -46,10 +46,13 @@ public class State {
     public static void main(String[] args) {
         HashSet<State>s= new HashSet<>();
         s.add(new State(new Pair(1,1),1,1,1,4));
-        State state=new State(new Pair(1,1),1,1,1,3);
+        State state=new State(new Pair(1,1),1,1,1,5);
         if(!s.contains(state)){
+            System.out.println(" not equal");
             s.add(state);
+
         }
+        else System.out.println("equal");
         System.out.println(s.size());
     }
 }
